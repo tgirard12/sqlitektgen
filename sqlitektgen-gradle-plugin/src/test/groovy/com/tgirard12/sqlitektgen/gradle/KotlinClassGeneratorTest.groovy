b5990638a7a0868e0d20230b2ae1aece87e76815
thomas <thomas.girard12@gmail.com>
2016-06-06 15:31:53 +0200

    Test with typeAppend

--- sqlitektgen-gradle-plugin/src/test/groovy/com/tgirard12/sqlitektgen/gradle/KotlinClassGeneratorTest.groovy
+++ sqlitektgen-gradle-plugin/src/test/groovy/com/tgirard12/sqlitektgen/gradle/KotlinClassGeneratorTest.groovy
@@ -34,7 +34,8 @@ public class KotlinClassGeneratorTest extends Specification {
           { "name": "string_defaultValue",  "ktType": "String", "defaultValue": "OK"    },
           { "name": "long_null",            "ktType": "Long?"                           },
           { "name": "float_default_value",  "ktType": "Float",  "defaultValue": "0"     },
-          { "name": "int_no_insert",        "ktType": "Int",    "defaultValue": "-1",   "insertOrUpdate": false } ]
+          { "name": "int_no_insert_pk",        "ktType": "Int",    "defaultValue": "-1",
+                "insertOrUpdate": false,  "typeAppend": "PRIMARY KEY AUTOINCREMENT"     } ]
 } ]"""
         def table = parser.parseJsonContent(json)
         def generateClazz = classGenerator.getKotlinClass(table[0])
@@ -51,7 +52,7 @@ data class my_table (
     val string_defaultValue: String = "OK",
     val long_null: Long? = null,
     val float_default_value: Float = 0,
-    val int_no_insert: Int = -1) {
+    val int_no_insert_pk: Int = -1) {
 
     constructor (cursor: Cursor) : this(
         string_null = cursor.getString(cursor.getColumnIndex(STRING_NULL)),
@@ -59,7 +60,7 @@ data class my_table (
         string_defaultValue = cursor.getString(cursor.getColumnIndex(STRING_DEFAULTVALUE)),
         long_null = cursor.getLong(cursor.getColumnIndex(LONG_NULL)),
         float_default_value = cursor.getFloat(cursor.getColumnIndex(FLOAT_DEFAULT_VALUE)),
-        int_no_insert = cursor.getInt(cursor.getColumnIndex(INT_NO_INSERT)))
+        int_no_insert_pk = cursor.getInt(cursor.getColumnIndex(INT_NO_INSERT_PK)))
 
     companion object {
         const val TABLE_NAME = "my_table"
@@ -68,7 +69,7 @@ data class my_table (
         const val STRING_DEFAULTVALUE = "string_defaultValue"
         const val LONG_NULL = "long_null"
         const val FLOAT_DEFAULT_VALUE = "float_default_value"
-        const val INT_NO_INSERT = "int_no_insert"
+        const val INT_NO_INSERT_PK = "int_no_insert_pk"
 
         const val CREATE_TABLE = \"\"\"CREATE TABLE my_table (
             string_null TEXT ,
@@ -76,7 +77,7 @@ data class my_table (
             string_defaultValue TEXT NOT NULL ,
             long_null INTEGER ,
             float_default_value REAL NOT NULL ,
-            int_no_insert INTEGER NOT NULL \n        )\"\"\"
+            int_no_insert_pk INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT\n        )\"\"\"
 
     }
 
